a036eaf28a75086a357e1c88051c9f19d630c573
Tarxio <tarxil@icloud.com>
2020-06-24 01:24:47 +0300

    Cyper-Pupper

---
+++ Blocks/header.php
@@ -0,0 +1,9 @@
+<style>
+    .fig {
+        text-align: center /* Выравнивание по центру */
+    }
+</style>
+
+<p class="fig">
+    <img class="d-block mx-auto mb-4" border="3px solid #FFDEAD" src="../img/1.png" alt="" width="100" height="100">
+</p>
