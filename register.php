ed13f3b0eddd9eb84fe8748cf94718b7bc76b87e
Tarxio <tarxil@icloud.com>
2020-06-29 01:01:58 +0300

    Переход на авторизацию в регистрации

--- register.php
+++ register.php
@@ -45,7 +45,7 @@ if(isset($_POST["register"])){
 <div class="container mregister">
     <div id="login">
         <h1>Регистрация</h1>
-        <form action="register.php" id="registerform" method="post" name="registerform">
+        <form action="login.php" id="registerform" method="post" name="registerform" action="login.php">
             <p><label for="user_login">Полное имя<br>
                     <input class="input" id="full_name" name="full_name"size="32"  type="text" value=""></label></p>
             <p><label for="user_pass">E-mail<br>
@@ -54,8 +54,9 @@ if(isset($_POST["register"])){
                     <input class="input" id="username" name="username"size="20" type="text" value=""></label></p>
             <p><label for="user_pass">Пароль<br>
                     <input class="input" id="password" name="password"size="32"   type="password" value=""></label></p>
-            <p class="submit" ><input class="button" id="register" name= "register" type="submit"  value="Зарегистрироваться"></p>
-            <p class="regtext">Уже зарегистрированы? <a href= "login.php">Введите имя пользователя</a>!</p>
+            <p class="submit"><input class="button" id="register" name= "register" type="submit" value="Зарегистрироваться"></p>
+            <p class="regtext">Уже зарегистрированы?</p>
+            <a href= "login.php">Авторизуйтесь!</a>
         </form>
     </div>
 </div>
